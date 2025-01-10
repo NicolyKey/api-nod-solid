d0f13eafb1beb6c9861f7ca44d48b56ea4f3c89c
Nicoly Araujo <nicolykeyssiane@gmail.com>
2025-01-10 16:33:04 -0300

    separating the functions to create a user and making it more dinamic

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { registerUseCase } from '@/services/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -13,25 +14,14 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, 2)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-
-    if (userWithSameEmail) {
+    try {
+        await registerUseCase({
+            name, email, password
+        })
+    } catch (e) {
         return reply.status(409).send()
     }
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash,
-        }
-    })
 
     return reply.status(201).send()
 }
